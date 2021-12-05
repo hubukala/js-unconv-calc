83b37fe0031768b4625c0d18116d066bb6855601
Hubert Bukala <hubertbukala@icloud.com>
2021-12-05 18:52:31 +0100

    filled app.js

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+const deafultResult = 0;
+
+let currentResult = deafultResult;
+
+currentResult = currentResult + 10 * 3;
+
+let calculationDescription = ` ${deafultResult} + 10 * 3`;
+
+outputResult(currentResult, calculationDescription);
