76b6d4d3e54cd2f6a11cf91d0e3fd16e54cb3619
Hubert Bukala <hubertbukala@icloud.com>
2021-12-10 07:54:45 +0100

    created function for adding numbers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,12 @@
 const deafultResult = 0;
-
 let currentResult = deafultResult;
+
+function sumNumbers(num1, num2){
+    const result = num1 + num2;
+    return result;
+}
+
+sumNumbers()
 
 currentResult = currentResult + 10 * 3;
 
