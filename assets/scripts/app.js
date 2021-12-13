0231f72053bafe8625923bf2fbbbb3bb429faa99
Hubert Bukala <hubertbukala@icloud.com>
2021-12-13 17:38:03 +0100

    added function to store data entries

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const deafultResult = 0;
 let currentResult = deafultResult;
+let dataEntries = [];
 
 function getNumberInput() {
     return parseInt(userInput.value);
@@ -10,32 +11,47 @@ function showOutput(operator, resultBefore, calcNumber) {
     outputResult(currentResult, calcShow);
 }
 
+function dataEntered(operationVal, prevnumVal, numVal, resultVal) {
+    entries = {
+        operation: operationVal,
+        prevnum: prevnumVal,
+        num: numVal,
+        result: resultVal
+    };
+    dataEntries.push(entries);
+    console.log(dataEntries);
+}
+
 function sumNumbers() {
     const numberInput = getNumberInput();
-    const initialREesult = currentResult;
-    currentResult = currentResult + numberInput;
-    showOutput('+', initialREesult, numberInput);
+    const initialResult = currentResult;
+    currentResult +=  numberInput;
+    showOutput('+', initialResult, numberInput);
+    dataEntered("ADD", initialResult, numberInput, currentResult);
 }
 
 function substractNumbers() {
     const numberInput = getNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult - numberInput;
+    currentResult -= numberInput;
     showOutput('-', initialResult, numberInput);
+    dataEntered("SUBSTRACT", initialResult, numberInput, currentResult);
 }
 
 function multiplyNumbers() {
     const numberInput = getNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult * numberInput;
+    currentResult *=  numberInput;
     showOutput('*', initialResult, numberInput);
+    dataEntered("MULTIPLY", initialResult, numberInput, currentResult);
 }
 
 function divideNumbers() {
     const numberInput = getNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult / numberInput;
+    currentResult /= numberInput;
     showOutput('/', initialResult, numberInput);
+    dataEntered("DIVIDE", initialResult, numberInput, currentResult);
 }
 
 addBtn.addEventListener('click', sumNumbers);
